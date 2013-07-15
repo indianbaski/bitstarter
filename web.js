7375a28f698e3aedaaf5c0a471ade758304dfb03
Baskaran <indianbaski@gmail.com>
2013-07-15 04:57:49 -0700

    First Real github upload

---
+++ web.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var fs = require('fs');
+
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+  
+fs.readFile('index.html', function(err, page) {
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.write(page);
+            response.end();
+        });
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
