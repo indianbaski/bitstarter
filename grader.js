591b329c64f17e1bc0ebe1781a1b348517526cc2
Baskaran <indianbaski@gmail.com>
2013-07-16 03:06:14 -0700

    Revised for Grader.js (accepting from url) & Check.json removed unwanted div

--- grader.js
+++ grader.js
@@ -22,53 +22,84 @@ References:
 */
 
 var fs = require('fs');
-var program = require('commander');
+var program = require('commander'); 
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
-var assertFileExists = function(infile) {
-    var instr = infile.toString();
-    if(!fs.existsSync(instr)) {
-        console.log("%s does not exist. Exiting.", instr);
-        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+function assertFileExists(filename) {
+    if (!fs.existsSync(filename)) {
+        console.log("%s does not exist. Exiting.", filename);
+        process.exit(1);
     }
-    return instr;
-};
+    return filename;
+}
+
+function loadChecks(checksfile) {
+    return JSON.parse(fs.readFileSync(checksfile)).sort();
+}
 
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
 };
 
-var loadChecks = function(checksfile) {
-    return JSON.parse(fs.readFileSync(checksfile));
-};
-
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
-    var checks = loadChecks(checksfile).sort();
+// It checks html
+function checkHtml(html, checks) {
+    $ = cheerio.load(html);
     var out = {};
     for(var ii in checks) {
         var present = $(checks[ii]).length > 0;
         out[checks[ii]] = present;
     }
     return out;
-};
+}
 
-var clone = function(fn) {
-    // Workaround for commander.js issue.
-    // http://stackoverflow.com/a/6772648
-    return fn.bind({});
-};
+function checkHtmlFile(filename, checks) {
+    return checkHtml(fs.readFileSync(filename), checks);
+}
 
-if(require.main == module) {
+// where error is null if there is no error
+function download(url, callback) {
+    var resp = rest.get(url);
+    resp.on('complete', function(result) {
+        if (result instanceof Error) {
+            // callback(result);
+            sys.puts('Error: ' + result.message);
+            this.retry(5000); // try again after 5 sec
+            return;
+        }
+        callback(null, result);
+    });
+}
+
+if (require.main == module) {
     program
-        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+        .option('-c, --checks <check_file>', 'Path to checks.json', assertFileExists, CHECKSFILE_DEFAULT)
+        .option('-f, --file <html_file>', 'Path to index.html', assertFileExists, HTMLFILE_DEFAULT)
+        .option('-u, --url  <url>', 'Path to downloaded url') ///////////////
+        .parse(process.argv); 
+
+    // this function loads checks & checks html
+    function check(err, html) {
+        if (err) {
+            console.log('Error getting html: ' + err);
+            process.exit(1);
+        }
+        var checks = loadChecks(program.checks);
+        var checkJson = checkHtml(html, checks);
+        var outJson = JSON.stringify(checkJson, null, 4);
+        console.log(outJson);
+    }
+
+    if (program.url) {
+        // download the provided url and then check the html
+        download(program.url, check);
+    } else if (program.file) {
+        // load html from a file and then check it
+        fs.readFile(program.file, check);
+    }
 } else {
-    exports.checkHtmlFile = checkHtmlFile;
+    exports.loadChecks = loadChecks; // for loading checks
+    exports.checkHtmlFile = checkHtmlFile; // for checking a file
 }
